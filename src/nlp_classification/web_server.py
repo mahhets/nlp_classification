99de1c7eb02ecf65354ba56a175bc2846396eb1c
mahh <m.kochetkov@savetime.net>
2021-09-13 02:53:10 +0300

    working rest api

--- src/nlp_classification/web_server.py
+++ src/nlp_classification/web_server.py
@@ -10,29 +10,48 @@ from environment_reference import EnvironmentReference
 load_dotenv()
 
 model_path = os.getenv(EnvironmentReference.SAVE_MODEL_PATH)
-print(model_path)
 
 with open(f'{model_path}MNB_model.pkl', 'rb') as f:
     model = pickle.load(f)
 
-print(model)
 
 app = Flask(__name__)
 
 
+@app.route("/", methods=["GET"])
+def general():
+    welcome = """REST API на основе ML-классификатора.\n
+    
+    Классифицирует тектовые сообщения по 2-м тематикам: Python / DataScience.\n\n
+    
+    Пример запроса: "server_adress/predict?text=В питоне очень приятный синтаксис"
+    """
+    return welcome
+
 @app.route('/predict', methods=['GET'])
-async def predict():
+def predict():
     text = str(request.args.get('text', ''))
     print(text)
     try:
         if not text:
             raise KeyError('Parameter "text" is required')
         data = {
-            'text': text
+            'text': [text]
         }
 
         temp_df = pd.DataFrame(data)
-        print(temp_df)
+        predict = model.predict(temp_df)[0]
+        if predict == 0:
+            return jsonify({"Chat": 'Python'}), 200
+        else:
+            return jsonify({"Chat": 'DS'}), 200
 
     except (KeyError, ValueError) as err:
         return jsonify({"error": str(err)}), 400
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.getenv('SERVER_HOST'),
+        port=os.getenv('SERVER_PORT')
+    )
